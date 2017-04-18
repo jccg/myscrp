0946c2be408eb2b1b0d0503c99b28d7b5eac97e5
jccg <logic90@qq.com>
2017-04-18 18:05:34 +0800

    Update r10.sh

--- zm/r10.sh
+++ zm/r10.sh
@@ -1,5 +1,19 @@
-docker run  -v `pwd`:/tmp/workdir   --net host  jrottenberg/ffmpeg  \
-        -i http://nobq.fvgt.online/$3$1 -stats \
-        -vf scale=-2:$2  -c:v libx264  -preset medium -tune film -crf 28 -c:a libfdk_aac -vbr 3 -movflags frag_keyframe+faststart -f mp4 -strict -2 -> $3$1
+if [ $2 == 0 ]
+then
+   echo "net file"
+   docker run  -v `pwd`:/tmp/workdir   --net host  jrottenberg/ffmpeg  \
+        -i http://nobq.fvgt.online/$4$1 -stats \
+        -vf scale=-2:$3  -c:v libx264  -preset medium -tune film -crf 28 \
+        -c:a libfdk_aac -vbr 3 -movflags frag_keyframe+faststart -f mp4 -strict -2 -> $4$1
+   MP4Box -isma $4$1
+else
+   echo "loc file"
+      docker run  -v `pwd`:/tmp/workdir   --net host  jrottenberg/ffmpeg  \
+        -i $4$1 -stats \
+        -vf scale=-2:$3  -c:v libx264  -preset medium -tune film -crf 28 \
+        -c:a libfdk_aac -vbr 3 -movflags frag_keyframe+faststart -f mp4 -strict -2 -> HA$4$1
+    MP4Box -isma HA$4$1
+fi
 
- MP4Box -isma $3$1
+
+
